cc10517542cdd8c3f578c4990c54641e4ce9e0e9
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2019-02-02 19:47:02 +0100

    Project setup

---
+++ exercise1/setup.R
@@ -0,0 +1,10 @@
+install.packages("knitr") #probably already installed
+install.packages("rmarkdown") #probably already installed
+install.packages("GLMsData") #data set for Problem 1
+install.packages("ggplot2") #plotting with ggplot (all code provided)
+install.packages("nortest") #test that data comes from normal distribution
+install.packages("class")# for function knn
+install.packages("colorspace") #for nice colors in provided plotting code
+install.packages("caret") #for confusion matrices
+install.packages("pROC") #for ROC curves
+install.packages("tidyverse")
