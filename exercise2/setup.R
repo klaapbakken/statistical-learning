2ccbb83a39dd1e4138e57fd42f7d0c5f5dce1ef2
Øyvind Klåpbakken <oyvind.klaapbakken@gmail.com>
2019-03-18 12:21:54 +0100

    Exercise template and setup (package installation)

---
+++ exercise2/setup.R
@@ -0,0 +1,15 @@
+install.packages("knitr") #probably already installed
+install.packages("rmarkdown") #probably already installed
+install.packages("bestglm")# for subset selection with categorical variables
+install.packages("glmnet")# for lasso
+install.packages("tree") #tree
+install.packages("randomForest") #for random forest
+install.packages("ElemStatLearn") #dataset in Problem 2
+install.packages("BiocManager")
+BiocManager::install(c("pheatmap")) #heatmap in Problem 2
+install.packages("ggplot2")
+install.packages("GGally") # for ggpairs
+install.packages("caret") #for confusion matrices
+install.packages("pROC") #for ROC curves
+install.packages("e1071") # for support vector machines
+install.packages("nnet") # for feed forward neural networks
